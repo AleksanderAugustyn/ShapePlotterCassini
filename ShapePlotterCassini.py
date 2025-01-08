8c03b25fe23e91101e523583e52e95cd35af909c
Aleksander Augustyn <aleksander.augustyn@ncbj.gov.pl>
2025-01-08 21:12:47 +0100

    ``` fix: Remove debug print and add volume calculations (master)
    Removed a debug print statement that was outputting the
    sphere volume.
    
    Added print statements to output the sphere volume,
    calculated volume, volume fixing factor, center of mass,
    recalculated volume, and the difference between the
    recalculated volume and the sphere volume.
    ```
    
    Signed-off-by: Aleksander Augustyn <aleksander.augustyn@ncbj.gov.pl>

--- ShapePlotterCassini.py
+++ ShapePlotterCassini.py
@@ -133,7 +133,6 @@ class CassiniShapeCalculator:
     def calculate_sphere_volume(self) -> float:
         """Calculate the volume of a sphere with the same number of nucleons."""
         R_0 = self.params.r0 * (self.params.nucleons ** (1/3))
-        print((4 / 3) * np.pi * R_0 ** 3)
         return (4/3) * np.pi * R_0**3
 
 
@@ -203,12 +202,25 @@ class CassiniShapePlotter:
         # Calculate volume fixing factor
         volume_fixing_factor = calculator.calculate_sphere_volume() / calculator.calculate_volume()
 
+        print(f"Sphere volume: {calculator.calculate_sphere_volume()}")
+        print(f"Volume: {calculator.calculate_volume()}")
+        print(f"Volume fixing factor: {volume_fixing_factor}")
+
         # Calculate center of mass
         z_cm_bar = calculator.calculate_zcm()
 
         # Transform rho_bar and z_bar to rho and z
         rho = rho_bar / volume_fixing_factor  # Scale the shape
         z = (z_bar - z_cm_bar) / volume_fixing_factor  # Center the shape
+
+        # Recalculate z_cm for the new shape, should be close to 0
+        z_cm = calculator.calculate_zcm()
+        print(f"Center of mass: {z_cm}")
+
+        # Recalculate volume, should be close to sphere volume
+        volume = calculator.calculate_volume()
+        print(f"Volume: {volume}")
+        print(f"Difference: {volume - calculator.calculate_sphere_volume()}")
 
         # Create reference sphere
         R_0 = self.nuclear_params.r0 * (self.nuclear_params.nucleons ** (1/3))
